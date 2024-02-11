cfb6ea19af945a938effc5ee7831a43fb832d68c
DESKTOP-A2SKAT1\Дом <VanaVanaKireev@mail.ru>
2024-02-12 00:49:50 +0400

    Осталось обновить svg в блоках "Как это работает" + "О платформе"

---
+++ src/components/common/mobile-header-navigate/mobile-header-navigate.js
@@ -0,0 +1,49 @@
+const mobileHeaderMenuIcon = document.querySelector('.mobile-header__menu-icon');
+const mobileHeader = document.querySelector('.mobile-header__menu');
+
+function isClickInsideElement(event, element) {
+  let container = event.target;
+  while (container != null) {
+    if (container === element) {
+      return true;
+    }
+    container = container.parentNode;
+  }
+  return false;
+}
+
+document.addEventListener('click', (event) => {
+  if (!isClickInsideElement(event, mobileHeader) && mobileHeaderMenuIcon.style.transform !== '') {
+    mobileHeaderMenuIcon.style.transform = '';
+    mobileHeader.style.display = 'none';
+  }
+}, false);
+
+mobileHeaderMenuIcon.addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (mobileHeaderMenuIcon.style.transform === '') {
+    mobileHeaderMenuIcon.style.transform = 'rotate(90deg)';
+    mobileHeader.style.display = 'flex';
+  } else {
+    mobileHeaderMenuIcon.style.transform = '';
+    mobileHeader.style.display = 'none';
+  }
+});
+
+export function markActiveHeaderLink() {
+  const links = document.querySelectorAll('.mobile-header__main-menu-item-link');
+
+  if (window.location.pathname === '/') {
+    links[0].classList.add('header__main-menu-item-link_this-page');
+    return;
+  }
+
+  for (let i = 1; i <= links.length; i++) {
+    if (window.location.pathname.includes(links[i].dataset.pathname)) {
+      links[i].classList.add('header__main-menu-item-link_this-page');
+      break;
+    }
+  }
+}
+
+markActiveHeaderLink();
